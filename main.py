9cc5a5991bcff8fff9b7239ddb4d34dcf27020a2
Błażej <blazdij7@gmail.com>
2024-02-22 23:03:33 +0100

    Some buttons

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ def write(char):
     entry.config(state="disabled")
 
 
+def cls():
+    entry.config(state="normal")
+    entry.delete('1.0', tk.END)
+    entry.config(state="disabled")
+
+
+def backspace():
+    pass
+
+
 root = tk.Tk()
 root.geometry("350x550")
 root.title("Calculator")
@@ -27,10 +37,33 @@ entryframe.grid_propagate(False)
 entry = tk.Text(entryframe, state="disabled", font=entryfont, cursor="arrow")
 entry.grid()
 
+# Buttons
 buttonframe = tk.Frame(mainframe, bg="red")
-buttonframe.pack()
+buttonframe.pack(fill="both")
+
 
 button1 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="(", font=buttonfont, command=lambda: write("("))
 button1.grid(row=0, column=0)
+
+button2 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text=")", font=buttonfont, command=lambda: write(")"))
+button2.grid(row=0, column=1)
+
+button3 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="CLS", font=buttonfont, command=cls)
+button3.grid(row=0, column=2)
+
+button4 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="BACK", font=buttonfont, command=backspace)
+button4.grid(row=0, column=3)
+
+button5 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="7", font=buttonfont, command=lambda: write("7"))
+button5.grid(row=1, column=0)
+
+button6 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="8", font=buttonfont, command=lambda: write("8"))
+button6.grid(row=1, column=1)
+
+button7 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="9", font=buttonfont, command=lambda: write("9"))
+button7.grid(row=1, column=2)
+
+button8 = tk.Button(buttonframe, height=int(buttonsize/2), width=buttonsize, text="/", font=buttonfont, command=lambda: write("/"))
+button8.grid(row=1, column=3)
 
 root.mainloop()
