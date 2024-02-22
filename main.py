c537328650903775f7d22e45db268684dbbed416
Błażej <blazdij7@gmail.com>
2024-02-22 22:17:41 +0100

    Working window

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("Bye World!")
-print("Bye world")
-print("Tak")
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("350x550")
+root.title("Calculator")
+root.resizable(False, False)
+
+
+
+root.mainloop()
